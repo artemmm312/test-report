7b2b946142e3840a25132cea9e038d99495fcd00
artemmm312 <artemmm312@gmail.com>
2022-09-29 18:08:52 +0300

    work2

---
+++ test-report/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Данные по сделкам", 
+		"/test-report/TransactionData/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Установка пользователей", 
+		"/test-report/InstallingUsers/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
